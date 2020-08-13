637e8602195a08d3ff61e8c82d5c6d1e5f118ee5
maru <marudev4905@gmail.com>
2020-08-13 13:02:42 +0900

    scroll 시 header style 변경 code 추가

---
+++ js/scroll.js
@@ -0,0 +1,46 @@
+const scroll = function(){
+    let 
+        nowScroll,
+        lastScroll,
+        headerCheck,
+        delta;
+
+    const header = document.getElementById("header");
+
+    const initModule = function(){
+        lastScroll = 0;
+        headerCheck = header.classList.contains("show");
+        delta = false;
+        _addEventHandlers();
+    }
+    const _addEventHandlers = function(){
+        window.addEventListener("scroll", _checkPosition);
+        window.addEventListener("load", _checkPosition);
+        window.addEventListener("resize", initModule);
+    }
+    const _checkPosition = function(){
+        _changeHeaderPosition();
+    }
+    const _changeHeaderPosition = function(){
+        nowScroll = window.scrollY;
+
+        if(nowScroll == 0){
+            if(delta){
+                header.classList.remove("hide");
+                header.classList.add("show");
+                delta = false;
+            }
+        }else if(nowScroll > 0){
+            if(delta == false){
+                header.classList.remove("show");
+                header.classList.add("hide");
+                delta = true;
+            }
+        }
+        lastScroll = window.scrollY;
+    };
+    return {
+        init : initModule
+    }
+}
+scroll().init();
